b39f68f07a4327ecbe08c06139c31bd2f4f6f374
tlchoque <tlchoque@gmail.com>
2017-07-22 20:42:23 -0500

    firs commit

---
+++ Source.cpp
@@ -0,0 +1,49 @@
+#include<iostream>
+#include<vector>
+#include<string>
+
+class cavityMap{
+	int n;
+	std::vector<std::string> grid;
+public:
+	cavityMap() {}
+	void read() {
+		std::cin >> n;
+		std::string s;
+		for (int i = 0; i < n; ++i) {
+			std::cin >>s;
+			grid.push_back(s);
+		}
+	}
+
+	void find_cavities() {
+		if (n < 3) return;
+		int m = n - 1;
+		for (int i = 1; i < m; ++i) {
+			for (int j = 1; j < m; ++j) {
+				if ( grid[i][j] >  grid[i - 1][j] &&
+					 grid[i][j] >  grid[i + 1][j] &&
+					 grid[i][j] >  grid[i][j - 1] &&
+					 grid[i][j] >  grid[i][j + 1] ){
+						grid[i][j] = 'X';
+				}
+			}
+		}
+	}
+
+	void print() {
+		std::cout << std::endl;
+		for (unsigned int i = 0; i < grid.size(); ++i)
+			std::cout << grid[i] << std::endl;
+	}
+};
+
+
+void main() {
+	cavityMap p;
+	p.read();
+	p.find_cavities();
+	p.print();
+
+	return;
+}
